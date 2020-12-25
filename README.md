1c9a59ea98b976b78c99f47f93686160c210f887
ste-giraldo <stefano@nerv.it>
2020-12-25 19:17:08 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -5,9 +5,6 @@
 script for backup cisco configs
 Tested on python 2.7 with paramiko lib
 
-#cisco_backup_v0.1.py 
-version for simple backup for one device
-
 #cisco_backup_v0.2.py 
 version for multiple backup from list of host from file hosts
 
--- cisco_backup_v0.2.py
+++ cisco_backup_v0.2.py
@@ -9,16 +9,16 @@ import datetime
 now = datetime.datetime.now()
 
 #authentication
-USER = 'user'
-PASSWORD = 'password'
-secret = 'password'
+USER = ''
+PASSWORD = ''
+secret = ''
 
 #start FOR ...in 
 f = open('cisco_hosts')
 for ip in f.readlines():
 	ip = ip.strip()
 	#prefix files for backup
-	filename_prefix ='cisco_backup' + ip
+	filename_prefix ='config_backup-' + ip
 	
 	#session start
 	client = paramiko.SSHClient()
@@ -27,21 +27,29 @@ for ip in f.readlines():
 
 	#ssh shell
 	chan = client.invoke_shell()
-	time.sleep(2)
+	time.sleep(0.5)
 	#enter enable secret
-	chan.send('en\n')
+	chan.send('enable\n')
 	chan.send(secret +'\n')
-	time.sleep(1)
+	time.sleep(0.5)
 	#terminal lenght for no paging 
 	chan.send('term len 0\n')
-	time.sleep(1)
-	#show config and write output
+	time.sleep(0.5)
+	#check software version
+	chan.send('show ver\n')
+	time.sleep(0.5)
+	#check files in flash
+	chan.send('dir\n')
+	time.sleep(0.5)
+	#show running-config and write output in a file
 	chan.send('sh run\n')
-	time.sleep(10)
-	output = chan.recv(99999)
+	time.sleep(3)
+	output = chan.recv(99999999)
 	#show output config and write file with prefix, date and time
 	print output
-	filename = "%s_%.2i-%.2i-%i_%.2i-%.2i-%.2i" % (filename_prefix,now.day,now.month,now.year,now.hour,now.minute,now.second)
+        #choose file format by commenting or decommenting lines below
+#	filename = "%s_%.2i-%.2i-%i_%.2i-%.2i-%.2i" % (filename_prefix,now.day,now.month,now.year,now.hour,now.minute,now.second)
+	filename = "%s_%.2i-%.2i-%i" % (filename_prefix,now.year,now.month,now.day)
 	ff = open(filename, 'a')
 	ff.write(output)
 	ff.close()
